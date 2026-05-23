da33d10382f0f9d4795a0ae1419f56b28e9c61ae
nekename <63245705+nekename@users.noreply.github.com>
2026-05-23 16:24:07 +0100

    chore: apply existing Prettier formatter config consistently

--- Plugins/Discord/propertyInspector/pushtomute/index.js
+++ Plugins/Discord/propertyInspector/pushtomute/index.js
@@ -39,7 +39,9 @@
 // $local 是否国际化
 // $back 是否自行决定回显时机
 // $dom 获取文档元素 - 不是动态的都写在这里面
-const $local = true, $back = false, $dom = {
+const $local = true,
+  $back = false,
+  $dom = {
     main: $('.sdpi-wrapper'),
     authorizeBtn: $('#authorizeBtn'),
     authorizeBox: $('#authorizeBox'),
@@ -47,41 +49,38 @@ const $local = true, $back = false, $dom = {
     initiateBoxx: $('#initiateBoxx'),
     clientId: $('#clientId'),
     open: $('#open'),
-};
+  };
 const $propEvent = {
-    didReceiveSettings(data) {
-
-    },
-    sendToPropertyInspector(data) {
-
-        // console.log(data);
-        if ('access_token' in data) {
-            if (!data.access_token) {
-                $dom.authorizeBox.style.display = 'block'
-            } else {
-                $dom.authorizeBox.style.display = 'none'
-            }
-        }
-        if ('status' in data) {
-            $dom.authorizeBox.style.display = 'block'
-        }
-        if (data?.msg == "RPC_CONNECTION_TIMEOUT") {
-            $dom.initiateBoxx.style.display = 'flex'
-        }
-        if (!data.initiate) {
-            $dom.initiateBox.style.display = 'flex'
-        } else {
-            $dom.initiateBox.style.display = 'none'
-        }
+  didReceiveSettings(data) {},
+  sendToPropertyInspector(data) {
+    // console.log(data);
+    if ('access_token' in data) {
+      if (!data.access_token) {
+        $dom.authorizeBox.style.display = 'block';
+      } else {
+        $dom.authorizeBox.style.display = 'none';
+      }
     }
+    if ('status' in data) {
+      $dom.authorizeBox.style.display = 'block';
+    }
+    if (data?.msg == 'RPC_CONNECTION_TIMEOUT') {
+      $dom.initiateBoxx.style.display = 'flex';
+    }
+    if (!data.initiate) {
+      $dom.initiateBox.style.display = 'flex';
+    } else {
+      $dom.initiateBox.style.display = 'none';
+    }
+  },
 };
 
 $dom.authorizeBtn.on('click', (e) => {
-    if ($dom.clientId.value != null && $dom.clientId.value != '') {
-        $websocket.openUrl("http://127.0.0.1:26432/authorization?clientId=" + $dom.clientId.value)
-    }
-})
+  if ($dom.clientId.value != null && $dom.clientId.value != '') {
+    $websocket.openUrl('http://127.0.0.1:26432/authorization?clientId=' + $dom.clientId.value);
+  }
+});
 
 $dom.open.on('click', () => {
-    $websocket.openUrl('https://discord.com/developers/applications')
-})
+  $websocket.openUrl('https://discord.com/developers/applications');
+});
